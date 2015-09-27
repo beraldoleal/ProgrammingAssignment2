0208bdd8227bfc67fc382ec9e1b4d47257642b99
Beraldo Leal <beraldo@beraldoleal.com>
2015-09-27 15:11:24 -0300

    added the solution for inverse matrix with cache query

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Cache inverse matrix
 
-## Write a short comment describing this function
+## This solution is based on template provided by assignment description.
+
+## This function creates a matrix but also can store the inverse on cache.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inverse <- NULL
+    set <- function(y) {
+        x <<- y
+        inverse <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(solution) inverse <<- solution
+    getinverse <- function() inverse
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
+## This function solves the inverse of a matrix. But firt it checks if there a
+## stored solution in cache. If yes, return the stored solution, else make the
+## computation.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inverse <- x$getinverse()
+  if (!is.null(inverse)) {
+    message("Returning data from cache")
+    return(inverse)
+  }
+
+  # Here there is no solution on cache. So, lets calculate.
+  data <- x$get()
+  # Solve the problem
+  inverse <- solve(data, ...)
+  # Stores the inverse into cache
+  x$setinverse(inverse)
+  # Return the inverse
+  inverse
 }
